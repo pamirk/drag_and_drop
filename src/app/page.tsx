c7db1ff3cdcb9a9e127d5880d23d047bfe8fb7e5
Pamir khan <pmysteryk@gmail.com>
2023-07-22 23:56:00 +0500

    feat: simple mvp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,132 @@
-import Image from 'next/image'
+"use client";
 import styles from './page.module.css'
+import {useState} from "react";
+import Image from "next/image";
+
+interface Todo {
+    id: number;
+    text: string;
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [todos, setTodos] = useState<Todo[]>([
+        {id: 1, text: 'Learn React'},
+        {id: 2, text: 'Learn Python'},
+        {id: 3, text: 'Learn Flask'}
+    ]);
+    const [inputValue, setInputValue] = useState<string>('');
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const appendTodo = (todo: string) => {
+        const newTodo: Todo = {
+            id: Date.now(),
+            text: todo,
+        };
+        setTodos((prevTodos) => [...prevTodos, newTodo]);
+    };
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    const destroyTodo = (id: number) => {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (inputValue.trim() === '') return;
+        appendTodo(inputValue);
+        setInputValue('');
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    const handleDragStart = (event: React.DragEvent<HTMLLIElement>, todoIndex: number) => {
+        event.dataTransfer.setData('text/plain', String(todoIndex));
+        event.dataTransfer.effectAllowed = 'move';
+    };
+
+    const handleDrop = (event: React.DragEvent<HTMLImageElement>) => {
+        event.preventDefault();
+        event.currentTarget.classList.remove(styles.shake);
+        const todoIndex = Number(event.dataTransfer.getData('text/plain'));
+        destroyTodo(todoIndex);
+    };
+
+    const handleDragEnter = (event: React.DragEvent<HTMLImageElement>) => {
+        event.currentTarget.classList.add(styles.shake);
+    };
+
+    const handleDragLeave = (event: React.DragEvent<HTMLImageElement>) => {
+        event.currentTarget.classList.remove(styles.shake);
+    };
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <p>Drag and drop to move items from todo list to dustbin</p>
+            </div>
+            <div>
+                <div className={styles.center}>
+                    <div className="row" style={{width: '100vw', textAlign: 'center'}}>
+                        <div className="col-sm-4">
+                            <form noValidate onSubmit={handleFormSubmit}>
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        placeholder="What do you want to do?"
+                                        className="form-control"
+                                        value={inputValue}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                                <input
+                                    type="submit"
+                                    value="Add Todo"
+                                    className="btn-primary btn-lg btn-block btn btn-default"
+                                />
+                            </form>
+                        </div>
+                        <div className="col-sm-4">
+                            <ol className="list-group">
+                                {todos.map((todo) => (
+                                    <li
+                                        key={todo.id}
+                                        className="list-group-item"
+                                        draggable={true}
+                                        onDragStart={(event) => handleDragStart(event, todo.id)}
+                                    >
+                                        {todo.text}
+                                    </li>
+                                ))}
+                            </ol>
+                        </div>
+                        <div className="col-sm-4">
+                            <Image
+                                src="/trash.png"
+                                onDragOver={(event) => event.preventDefault()}
+                                onDrop={handleDrop}
+                                onDragEnter={handleDragEnter}
+                                onDragLeave={handleDragLeave}
+                                alt="Trash"
+                                width={100}
+                                height={100}
+                            />
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+            <div className={styles.grid}>
+                <a
+                    href="https://pamirk.github.io/"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <p>Coded by</p>
+
+                    <h2>PK <span>-&gt;</span></h2>
+                </a>
+            </div>
+        </main>
+    )
 }
